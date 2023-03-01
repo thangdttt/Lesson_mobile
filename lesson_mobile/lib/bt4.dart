ccf5df282689ba7c9410dcf37715a76ca3d1d485
thang <thangdtttk12b$gmail.com>
2023-03-01 16:18:01 +0700

    my page

--- lesson_mobile/lib/bt4.dart
+++ lesson_mobile/lib/bt4.dart
@@ -44,6 +44,23 @@ class HomeStartPage extends State<Lesson3HomePage> {
               '$soLanBam',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://scontent.fhan5-11.fna.fbcdn.net/v/t1.6435-9/81464373_818049705305077_889717121204879360_n.jpg?stp=dst-jpg_s600x600&_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=EDjtO_NgVlEAX8yz9V5&tn=2xK6PuYdA34ShfSZ&_nc_ht=scontent.fhan5-11.fna&oh=00_AfD8WpKv9lSIUXYHWMxq0ymfEacu8fHGpc75i83W3Q-meg&oe=64266C2B',
+                  width: 300,
+                  height: 300,
+                )
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('Nút đã bấm')));
+              },
+              child: Text('click'),
+            ),
           ],
         )),
       ),
